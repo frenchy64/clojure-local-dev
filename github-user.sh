7d5337cde876fef1e6e909cbee3d5fcf5ea61f4a
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-01-29 12:53:20 -0500

    repl script

---
+++ github-user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Print your GitHub user name
+
+GITHUB_USER=$(git ls-remote --get-url origin | bb -i '(some->> *input* first (re-matcher #"git@github.com:(.*)/clojure-local-dev.git") re-find second println)')
+
+if [[ -z "${GITHUB_USER}" ]]; then
+  echo "Something went wrong inferring your GitHub user."
+  echo "Make sure the `origin` remote of your clojure-local-dev repo"
+  echo "is named git@github.com:<YOUR_GH_USER>/clojure-local-dev.git"
+  exit 1
+fi
+
+echo "${GITHUB_USER}"
---
+++ repl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Start an (n)REPL in the current branch.
+#
+# For testing purposes only -- restart the REPL to
+# reload changes to Clojure's src.
+#
+# eg., my-branch$ ../repl.sh
+#
+# See also: https://clojure.org/dev/developing_patches#_run_an_individual_test
+
+set -xe
+
+mvn -Dmaven.test.skip=true clean package
+./antsetup.sh
+
+clj -A:nrepl:dbg
