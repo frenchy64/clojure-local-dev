fcb8b45df1751b7e833d8a5223a0430566aa3c1e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-08-28 20:47:42 -0400

    doc

--- prep-clojure-cli.sh
+++ prep-clojure-cli.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Prepare Clojure repository for a Clojure CLI invocation. Run in branch directory.
+#
+# eg., ../prep-clojure-cli.sh
 
 cp ../deps.edn .
 mvn -Dmaven.test.skip=true clean package
