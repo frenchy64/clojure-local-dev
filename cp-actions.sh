19a8708bc7d0df5a3f70d54ebe7a6a28ba84722f
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-04-21 11:26:43 -0500

    clojure has an actions build now

--- cp-actions.sh
+++ cp-actions.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-mkdir -p .github/workflows
-cp ../build.yml .github/workflows
 cp ../deps.edn .
 cat ../extra-gitignore >> .gitignore
-git add -f .github/workflows/build.yml deps.edn
+git add -f deps.edn
